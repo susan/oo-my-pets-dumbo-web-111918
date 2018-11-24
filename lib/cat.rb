d077689e8feaa5a53874da65804b6c61cbeff874
Susan Isserman <bluenote@outlook.com>
2018-11-24 22:04:36 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,10 +1,10 @@
 class Cat
   attr_accessor :mood, :owner
-  attr_reader :name 
+  attr_reader :name
 
   def initialize(name)
   	@name = name
   	#mood = nervous #don't need mood if have instance variable @mood
   	@mood = "nervous"
-  end	
+  end
 end
